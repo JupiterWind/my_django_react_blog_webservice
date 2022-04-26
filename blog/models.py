db48f1941604217f60b89aa1649bd51e03e2c35a
JupiterWind <hasiyara@naver.com>
2022-04-26 20:57:27 +0900

    모델추가

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+'''
+- Post : title, hook_text, content, created_at, updated_at, author, tags
+- Tag : name, slug
+- Comment : author, post, content, created_at, modified_at
+'''
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    # 사람이 읽을 수 있는 텍스트로 고유 URL을 만들고 싶을 때
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title} :: '
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'::{self.content}'
+    
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+
+
+
